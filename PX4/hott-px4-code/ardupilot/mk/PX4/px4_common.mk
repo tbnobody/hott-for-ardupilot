0918977db3931554dbdb7f416b0a0807369ddbb7
Thomas Basler <thomas@familie-basler.net>
2015-05-03 13:41:39 +0200

    Changed location of MODULES configruation

--- PX4/hott-px4-code/ardupilot/mk/PX4/px4_common.mk
+++ PX4/hott-px4-code/ardupilot/mk/PX4/px4_common.mk
@@ -1,12 +1,11 @@
 #
-# Makefile for the px4fmu-v2_APM configuration
+# common makefile elements for all PX4 boards
 #
 
 #
 # Use the configuration's ROMFS.
 #
 ROMFS_ROOT	 = $(SKETCHBOOK)/mk/PX4/ROMFS
-
 MODULES		+= $(APM_MODULE_DIR)
 MODULES		+= ./../../hott-px4-code/px4-hott-module
 
@@ -20,23 +19,25 @@ MODULES		+= drivers/stm32/tone_alarm
 MODULES		+= drivers/led
 MODULES		+= drivers/px4fmu
 MODULES		+= drivers/px4io
-MODULES		+= drivers/boards/px4fmu-v2
+MODULES		+= drivers/px4flow
 MODULES		+= drivers/rgbled
-MODULES		+= drivers/lsm303d
-MODULES		+= drivers/l3gd20
+MODULES		+= drivers/oreoled
 MODULES		+= drivers/mpu6000
 MODULES		+= drivers/hmc5883
 MODULES		+= drivers/ms5611
 MODULES		+= drivers/mb12xx
-MODULES		+= drivers/gps
-MODULES		+= drivers/hil
+MODULES		+= drivers/ll40ls
+MODULES		+= drivers/trone
+#MODULES	+= drivers/gps
+#MODULES	+= drivers/hil
 #MODULES	+= drivers/hott_telemetry
-MODULES		+= drivers/blinkm
+#MODULES	+= drivers/blinkm
 #MODULES	+= modules/sensors
 MODULES		+= drivers/airspeed
 MODULES		+= drivers/ets_airspeed
 MODULES		+= drivers/meas_airspeed
 MODULES		+= drivers/mkblctrl
+MODULES		+= drivers/batt_smbus
 
 #
 # System commands
@@ -48,10 +49,20 @@ MODULES		+= systemcmds/perf
 MODULES		+= systemcmds/pwm
 MODULES		+= systemcmds/reboot
 MODULES		+= systemcmds/top
-MODULES		+= systemcmds/tests
+#MODULES	+= systemcmds/tests
 MODULES		+= systemcmds/nshterm
-MODULES		+= systemcmds/auth
 MODULES         += systemcmds/mtd
+MODULES         += systemcmds/ver
+
+ifneq ($(wildcard $(PX4_ROOT)/src/systemcmds/reflect),)  
+MODULES         += systemcmds/reflect
+endif
+ifneq ($(wildcard $(PX4_ROOT)/src/systemcmds/motor_test),)  
+MODULES         += systemcmds/motor_test
+endif
+ifneq ($(wildcard $(PX4_ROOT)/src/systemcmds/usb_connected),)  
+MODULES         += systemcmds/usb_connected
+endif
 
 #
 # Library modules
@@ -60,9 +71,17 @@ MODULES		+= modules/systemlib
 MODULES		+= modules/systemlib/mixer
 MODULES		+= modules/uORB
 MODULES		+= lib/mathlib/math/filter
-MODULES	        += modules/libtomfastmath
-MODULES         += modules/libtomcrypt
 
+# Note: auth disabled to keep us under 1MB flash because of STM32 bug
+#ifneq ($(wildcard $(PX4_ROOT)/src/systemcmds/auth),)  
+#MODULES		+= systemcmds/auth
+#endif
+#ifneq ($(wildcard $(PX4_ROOT)/src/modules/libtomfastmath),)  
+#MODULES	        += modules/libtomfastmath
+#MODULES         += modules/libtomcrypt
+#endif
+
+MODULES		+= lib/conversion
 
 #
 # Transitional support - add commands from the NuttX export archive.
